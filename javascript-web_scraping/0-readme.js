1b0c8ee78b0af3656b00930e28a812242da8fb0e
christophemr <christophemr59@gmail.com>
2024-05-14 10:42:49 +0200

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', function(err, data) {
-  if (err) {
-    console.log(err);
+fs.readFile(filePath, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
   } else {
-    console.log(data);
+    console.log(content);
   }
 });
